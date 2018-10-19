4879f9aae1ac7e15551ab2b2db4a2fea03856df0
tyler-vs <tylervucf@knights.ucf.edu>
2018-10-19 19:27:01 -0400

    feat: Add new test suite named "Initial Entries"

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -104,6 +104,8 @@ $(function() {
     });
 
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function() {
+
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -118,4 +120,5 @@ $(function() {
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+    });
 }());
