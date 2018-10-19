b6e156e378d7f12afacebaa8c727367fe8f62a24
tyler-vs <tylervucf@knights.ucf.edu>
2018-10-19 19:21:41 -0400

    feat: Add test suite for the menu

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -67,6 +67,7 @@ $(function() {
 
 
     /* TODO: Write a new test suite named "The menu" */
+    describe('The menu', function() {
 
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
@@ -79,6 +80,7 @@ $(function() {
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+    });
 
     /* TODO: Write a new test suite named "Initial Entries" */
 
