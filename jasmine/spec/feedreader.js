b6b8468b3f19bfbf721cd21c72e710c2b9fb7eb2
tyler-vs <tylervucf@knights.ucf.edu>
2018-10-19 22:53:08 -0400

    feat: Add test suite for new feed entries

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -137,9 +137,11 @@ $(function() {
     });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
+    describe('New Feed Selection', function() {
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+    });
 }());
